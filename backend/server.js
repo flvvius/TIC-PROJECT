31fd537a0023bca813f608122b73fe6281d984ca
flavius <flaviuscojocaru19@gmail.com>
2024-12-18 10:56:10 +0200

    backend init

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log("Server is running");
+const express = require("express");
+const app = express();
+const port = 8080;
+
+// const httpLogger = require('morgan');
+// const cors = require('cors');
+// app.use(httpLogger('dev'));
+// app.use(cors())
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
